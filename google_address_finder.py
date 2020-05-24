dd413564e38765660f3d3cbec6fdaec45c37462e
ChandanChaman <41324382+ChandanChaman@users.noreply.github.com>
2020-05-24 10:25:19 +0100

    Geocode api
    What is an API ?
    API is an acronym for Application Programming Interface. It is a software intermediary that allows two applications to talk to each other. It acts as a middleman between any two machines that want to connect with each other for a specified task. It can be considered as a translator between two people who don’t speak the same language but can communicate using an intermediary.
    APIs communicate through a set of rules that define how computers, applications or machines can talk to each other. APIs help developers to easily add new features to their apps. Rather than building something from scratch. Some examples of API are :
    · Using Google Maps in a rideshare app — Several rideshare app like uber uses google map API in their app for location tracking. This way they don’t need to create a location tracking system and which saves lot of time & money.
    · Flight information between airlines and travel sites– Airlines companies uses API to share their flight information (like schedule, fare etc) with different travel sites.
    · YouTube videos into a webpage — Using API provided by youtube we can easily embed any video in a webpage.
     
    Here in below example, in which we are trying to fetch full address including Geographic coordinates for any input location .
    For example:
    My input is : Heathrow Airport , London
    Expected output is: Full address
    Heathrow Airport (LHR), Longford TW6, UK
    PostCode : 
    TW6
    Lattitude : 
    51.4700223
    Longitude : 
    -0.4542955
    Address Type : 
    airport, establishment, point_of_interest
    Pre-requisite:
    · Python 3 installed on machine— Download & install from here
    · Your own google api key—
    Steps to get free api key from google.
    Execution:
    · This code can provide the complete details of the any geographical location. e.g. complete address with zipcode, longitude, latitude etc.
    · Download & save the python code from here
    · Update the api_key with your own key at line number 5.
    · Now run the code from window cmd or mac/linux terminal.

---
+++ google_address_finder.py
@@ -0,0 +1,40 @@
+import urllib.request, urllib.parse
+import json
+
+# UPDATE HERE THE API KEY WITH YOUR OWN -
+api_key = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+
+api = 'https://maps.googleapis.com/maps/api/geocode/json?'
+while True:
+    location = input('Enter a Location to get complete address details (e.g.- "tajmahal, india)" or just enter to quit : ')
+    if len(location)<1:
+        print ('Good Bye')
+        break
+    print ('Fetching complete address . .. ...')
+    url= api+ urllib.parse.urlencode({'address':location,'key':api_key,'sensor':'false'})
+    uh= urllib.request.urlopen(url)
+    data = uh.read().decode()
+
+    try:
+        json_data=json.loads(data)
+    except:
+        print ('No json data Found in google.')
+
+    if not json_data or 'status' not in json_data or json_data['status'] != 'OK':
+        print ('No Address Found. Please enter valid address or Check if api key is working.')
+        continue
+
+#Extracting required field from raw data
+    comp_add=json_data['results'][0]['formatted_address']
+    post_code=json_data['results'][0]['address_components'][-1]['long_name']
+    lat=json_data['results'][0]['geometry']['location']['lat']
+    lng=json_data['results'][0]['geometry']['location']['lng']
+    type=json_data['results'][0]['types']
+
+    print ('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print ('Complete Address :', '\n','\t','\t', comp_add)
+    print ('PostCode :', '\n','\t','\t', post_code)
+    print ('Lattitude :', '\n','\t','\t', lat)
+    print ('Longitude :', '\n','\t','\t', lng)
+    print ('Address Type :', '\n','\t','\t', ', '.join(type) )
+    print ('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~','\n')
